ffc1230eb253478c7a38ae28503d30cbd5b962ae
Alberto Anceschi <anceschialberto@me.com>
2021-10-31 22:33:31 +0100

    remove unnecessary codecommit stuff

--- pipeline/lib/pipeline-stack.ts
+++ pipeline/lib/pipeline-stack.ts
@@ -1,6 +1,5 @@
 import * as cdk from '@aws-cdk/core';
 import s3 = require('@aws-cdk/aws-s3');
-import codecommit = require('@aws-cdk/aws-codecommit');
 import codepipeline = require('@aws-cdk/aws-codepipeline');
 import codepipeline_actions = require('@aws-cdk/aws-codepipeline-actions');
 import codebuild = require('@aws-cdk/aws-codebuild');
@@ -11,13 +10,6 @@ export class PipelineStack extends cdk.Stack {
 
     // The code that defines your stack goes here
     const artifactsBucket = new s3.Bucket(this, "ArtifactsBucket");
-
-    // Import existing CodeCommit sam-app repository
-    const codeRepo = codecommit.Repository.fromRepositoryName(
-      this,
-      'AppRepository', // Logical name within CloudFormation
-      'sam-app' // Repository name
-    );
 
     // Pipeline creation starts
     const pipeline = new codepipeline.Pipeline(this, 'Pipeline', {
